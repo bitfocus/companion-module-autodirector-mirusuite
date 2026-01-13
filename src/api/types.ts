4ed7dc0b65652c4a7349fd25090a48f1cfcac4fc
kleincode <f.kleinst10810@gmail.com>
2026-01-13 11:27:14 +0100

    remove "as" casts from actions.ts

--- src/api/types.ts
+++ src/api/types.ts
@@ -11,3 +11,17 @@ export type ShotSize = components['schemas']['ShotSize']
 export type TrackingMode = components['schemas']['TrackingMode']
 export type GUIUpdate = components['schemas']['GUIUpdate']
 export type ProjectEntity = components['schemas']['ProjectEntity']
+
+export function toShotSize(value: any): ShotSize | undefined {
+	if (['WIDE', 'MEDIUM', 'CLOSE_UP'].includes(value)) {
+		return value
+	}
+	return undefined
+}
+
+export function toTrackingMode(value: any): TrackingMode | undefined {
+	if (['ALL', 'MANUAL', 'SINGLE'].includes(value)) {
+		return value
+	}
+	return undefined
+}
